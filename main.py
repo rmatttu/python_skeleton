c414513afbfea8f6e7151dbaf0fe48d3159ba6c7
rmatttu <mggggk@gmail.com>
2018-04-12 13:41:24 +0900

    バグ修正
    infoログレベル以下が出力されないバグ修正
    設定ファイル名記述ミスを修正

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging.config
 
 def main():
     # ログ設定ファイルからログ設定を読み込み
-    logging.config.fileConfig('system/logging.conf')
+    logging.config.fileConfig('log.ini')
 
     logger = logging.getLogger()
 
@@ -17,7 +17,7 @@ def main():
     logger.warning('warning')
 
     conf = configparser.ConfigParser()
-    conf.read('./conf.ini', 'UTF-8')
+    conf.read('conf.ini', 'UTF-8')
 
     if conf.get('settings', 'bool_test') == "True":
         logger.info(conf.get('settings', 'nihongo'))
