6dbc07c10282b6c1c8442f2533b5bf60f64705b0
rmatttu <mggggk@gmail.com>
2018-04-26 10:09:35 +0900

    文字列囲み文字を統一

--- example.py
+++ example.py
@@ -6,24 +6,24 @@ import logging.config
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('src', default="src_dir", help='source directory (src_dir)')
-    parser.add_argument('dst', default="dst_dir", help='destination directory (dst_dir)')
-    parser.add_argument('--foo', default="test", help='optional string foo (test)')
-    parser.add_argument('--optional-num', default=5, type=int, help='intのみを受け付けるオプション (5)')
-    parser.add_argument('--sw1', action='store_true', help="switch 1, オプションを付けるとsw2はtrue (false)")
-    parser.add_argument('--sw2', action='store_false', help="switch 2, オプションを付けるとsw2はfalse (true)")
+    parser.add_argument("src", default="src_dir", help="source directory (src_dir)")
+    parser.add_argument("dst", default="dst_dir", help="destination directory (dst_dir)")
+    parser.add_argument("--foo", default="test", help="optional string foo (test)")
+    parser.add_argument("--optional-num", default=5, type=int, help="intのみを受け付けるオプション (5)")
+    parser.add_argument("--sw1", action="store_true", help="switch 1, オプションを付けるとsw2はtrue (false)")
+    parser.add_argument("--sw2", action="store_false", help="switch 2, オプションを付けるとsw2はfalse (true)")
     args = parser.parse_args()
 
     # ログ設定ファイルからログ設定を読み込み
-    logging.config.fileConfig('log.ini')
+    logging.config.fileConfig("log.ini")
     logger = logging.getLogger()
 
-    logger.log(20, 'info')
-    logger.log(30, 'warning')
-    logger.log(100, 'test')
+    logger.log(20, "info")
+    logger.log(30, "warning")
+    logger.log(100, "test")
 
-    logger.info('info')
-    logger.warning('warning')
+    logger.info("info")
+    logger.warning("warning")
 
     logger.debug("args")
     logger.debug("src = %s" % args.src)
@@ -34,11 +34,11 @@ def main():
     logger.debug("sw2 = %s" % args.sw2)
 
     conf = configparser.ConfigParser()
-    conf.read('conf.ini', 'UTF-8')
+    conf.read("conf.ini", "UTF-8")
 
-    if conf.get('settings', 'bool_test') == "True":
-        logger.info(conf.get('settings', 'nihongo'))
+    if conf.get("settings", "bool_test") == "True":
+        logger.info(conf.get("settings", "nihongo"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
